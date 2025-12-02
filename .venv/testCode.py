1b872319a9fa13c4edc6212c3a49bb0b9bf68d3c
LeenLann <vasil.05.3pryhodko@gmail.com>
2025-12-02 17:11:26 +0200

    ф-ція для аналізу ефективності

--- .venv/testCode.py
+++ .venv/testCode.py
@@ -9,43 +9,57 @@ class MemoryAndTimeTester:
         self.memoryes   = []
 
     def test(self, args : list|dict = None):
-        pos_args = [] # підготовка аргументів
+        pos_args = [] # підготовка аргументів (не обов'язково передавати щось)
         kw_args  = {}
         if   isinstance(args, list): pos_args = args
         elif isinstance(args, dict): kw_args  = args
 
-        tracemalloc.start()
+        tracemalloc.start() # початок підрахунків
         start = time.time()
         
         # виконання ф-ції
         result = self.func(*pos_args, **kw_args)
 
         fulTime = time.time() - start
-        snapshot = tracemalloc.take_snapshot()
-        peak = tracemalloc.get_traced_memory()[1]
-        tracemalloc.stop()
+        snapshot = tracemalloc.take_snapshot() # закінчення підрахунків
+        peak = tracemalloc.get_traced_memory()[1] # отримання пікового значення
+        tracemalloc.stop() 
 
-        stats = snapshot.statistics('lineno')
+        stats = snapshot.statistics('lineno') # отримання статистики по рядках кода
         memoryes = []
         useMemory = 0
-        for stat in stats:
+        for stat in stats: # збереження статистики
             useMemory += stat.size
             memoryes.append({"size": stat.size, "count": stat.count, "average": round(stat.size/stat.count, 2)})
         
+        # збереження іншийх даних
         self.memoryes.append({"useMemory": useMemory, "peak": peak, "stats": memoryes})
         self.times.append(fulTime)
         self.outputs.append(result)
 
+    def reset(self):
+        self.outputs    = []
+        self.times      = []
+        self.memoryes   = []
+
+    def setFunc(self, func):
+        self.func = func
+        self.reset()
+
 # tester.memoryes = [{'useMemory': (тут сума використання по рядкам ф-ції), 'peak': (пікове використання(надіюсь що пікове...)), 'stats': [{'size': (загальна кількість використано для рядка), 'count': (кількість використань), 'average': (середнє)}, ...]}]
 
-def something(n):
-    s = sum(list(range(n)))
-    return s
+def test():
+    def something(n):
+        s = sum(list(range(n)))
+        return s
 
-tester = MemoryAndTimeTester(something)
+    tester = MemoryAndTimeTester(something)
 
-tester.test([10000])
+    tester.test([10000])
 
-print(tester.outputs)
-print(tester.memoryes)
-print(tester.times)
+    print(tester.outputs)
+    print(tester.memoryes)
+    print(tester.times)
+
+if __name__ == "__main__":
+    test()
